06c4b4e46b4603f215a6669560e63d3314591516
Elisabeth Kollrack <elisabethkollrack@ip0af50f80.int.uvm.edu>
2025-09-15 12:34:31 -0400

    added firebase connection

--- admin.py
+++ admin.py
@@ -1,3 +1,10 @@
 # The admin program will read data from a JSON file saved locally and will initialize and upload the data
 # to a Google Firebase Cloud Datastore (not a Firebase Realtime Database). You’ll run this program one
 # time. If you run it a second time, it should delete and recreate the datastore.
+
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("mountains_private_key.json")
+firebase_admin.initialize_app(cred)
+
