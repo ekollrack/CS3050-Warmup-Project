c00692c4791649039657ba2d06e51b7e6ca363c9
ekollrack <elisabethkollrack@gmail.com>
2025-09-15 13:31:52 -0400

    cleaning up

--- admin.py
+++ admin.py
@@ -1,30 +1,22 @@
-# The admin program will read data from a JSON file saved locally and will initialize and upload the data
-# to a Google Firebase Cloud Datastore (not a Firebase Realtime Database). You’ll run this program one
-# time. If you run it a second time, it should delete and recreate the datastore.
-
 import firebase_admin
 from firebase_admin import credentials, firestore
 import json
+import sys
 
-cred = credentials.Certificate("mountains_private_key.json")
+json_file = sys.argv[1]
+
+cred = credentials.Certificate('/Users/elisabethkollrack/Desktop/CS 3050/mountains_private_key.json') # Need to fix
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
 
-json_file = "mountains.json"
 
-collection_name = "mountains"
+collection = db.collection("mountains")
 
-
-coll_ref = db.collection(collection_name)
-# Load JSON data
 with open(json_file, "r") as f:
     data = json.load(f)
 
-# Write JSON to Firestore
-for key, value in data.items():
-    coll_ref.document(key).set(value)
+for mountain in data:
+    doc_id = mountain["Mountain Name"]
+    collection.document(doc_id).set(mountain)
 
-print("Data uploaded successfully!")
-
-
+print("Data uploaded!")
