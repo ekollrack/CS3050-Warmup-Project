6c883bd862a88f3561ccbd9194552b473a34f742
Elisabeth Kollrack <134722513+ekollrack@users.noreply.github.com>
2025-09-15 12:08:32 -0400

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,1 +1,3 @@
-
+# The admin program will read data from a JSON file saved locally and will initialize and upload the data
+to a Google Firebase Cloud Datastore (not a Firebase Realtime Database). You’ll run this program one
+time. If you run it a second time, it should delete and recreate the datastore.
